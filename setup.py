29cab0646b3654299b64326d24526ed8ca84e3b0
Matt Boyer <mboyer@sdf.org>
2015-10-02 21:42:57 +0100

    Setup.py tidy-up

--- setup.py
+++ setup.py
@@ -6,30 +6,23 @@ setup(
     name='bitbucket-jekyll-hook',
     version=version.get_git_version(),
     url='https://github.com/mattboyer/bitbucket_jekyll_hook',
-    description='fdfd',
+    description=('BitBucket webhook to trigger a static site build through '
+        'Jekyll'),
     author='Matt Boyer',
     author_email='mboyer@sdf.org',
     license='BSD',
     classifiers=[
-        #'Environment :: Console',
-        #'Development Status :: 3 - Alpha',
-        #'Intended Audience :: Developers',
-        #'License :: OSI Approved :: BSD License',
-        #'Operating System :: POSIX',
-        #'Topic :: Software Development :: Version Control',
-        #'Programming Language :: Python :: 2.6',
-        #'Programming Language :: Python :: 2.7',
-        #'Programming Language :: Python :: 3.3',
-        #'Programming Language :: Python :: 3.4',
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: No Input/Output (Daemon)',
+        'Framework :: Flask',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
     ],
     keywords='git webhook bitbucket jekyll publish',
     packages=['BB_jekyll_hook'],
-    include_package_data=True,
-    data_files = [],
     install_requires=['flask'],
-    entry_points={
-        'console_scripts': [
-            'git-guilt = git_guilt.guilt:main'
-        ]
-    },
 )
